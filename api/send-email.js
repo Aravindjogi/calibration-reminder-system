6439732cd354137cb984dc4b92a3013ec7456a26
Aravindjogi <aravindjogi454@gmail.com>
2025-08-14 08:51:58 +0530

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
 async function getKVClient() {
   try {
     return createClient({
-      url: process.env.KV_REST_API_URL,
-      token: process.env.KV_REST_API_TOKEN,
+       url: process.env.https://ecfg_zerfdduvbcumr44psawcq9nnh2yb.kv.vercel-storage.com,
+      token: process.env.5bf6b008a9ec05f6870c476d10b53211797aa000f95aae344ae60f9b422286da,
     });
   } catch (err) {
     throw new Error('Failed to initialize KV client: ' + err.message);
@@ -82,3 +82,4 @@ module.exports = async (req, res) => {
     return res.status(500).json({ error: 'Failed to send email: ' + err.message });
   }
 };
+
